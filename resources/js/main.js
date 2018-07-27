d5d9c17818e068e19b0e0782c3844bf3125dc298
Brian Miranda <brianmiranda@outlook.com>
2018-07-27 13:01:39 -0500

    Problem setting up slick

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
