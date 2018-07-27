feca39381c3677147cad79139d447a2e78bd2dba
Brian Miranda <brianmiranda@outlook.com>
2018-07-27 13:36:08 -0500

    Create the Carousel

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
     autoplay: true,
-    speed: 4000,
+    autoplaySpeed: 4000,
     arrows: false,
     draggable: false,
     pauseOnFocus: false,
